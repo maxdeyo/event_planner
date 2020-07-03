bb3084b1a17b30835d2709070bc59b8e2f82d173
maxdeyo <maxdeyo@hawaii.edu>
2020-07-03 10:10:48 -1000

    changed ics file

--- index.js
+++ index.js
@@ -18,9 +18,22 @@ app.get('/api/test', (req, res) => {
   console.log(`Sent test`);
 });
 
-let icsText = "BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//hacksw/handcal//NONSGML v1.0//EN\nBEGIN:VEVENT\nUID:uid1@example.com\nDTSTAMP:19970714T170000Z\nORGANIZER:CN=John Doe:MAILTO:john.doe@example.com\nDTSTART:19970714T170000Z\nDTEND:19970715T035959Z\nSUMMARY:Bastille Day Party\nGEO:48.85299:2.46885\nEND:VEVENT\nEND:VCALENDAR";
 
-var text={"myfile.ics":icsText,"bye.txt":"Goodbye Cruel World!"};
+//let icsText = "BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//hacksw/handcal//NONSGML v1.0//EN\nBEGIN:VEVENT\nUID:uid1@example.com\nDTSTAMP:20200703T191855Z\nDTSTART:20200813T100000Z\nDTEND:20200813T130000Z\nSUMMARY:Study for exam\nLOCATION:Hamilton Library\nEND:VEVENT\nEND:VCALENDAR";
+
+let icsText = 'BEGIN:VCALENDAR' + '\n'
++'VERSION:2.0' + '\n' + 
+'BEGIN:VEVENT' + '\n' +
+'CLASS:PUBLIC' + '\n' +
+'DESCRIPTION:Study' + '\n' +
+'DTSTART:20200813T200000Z' + '\n' +
+'DTEND:20200813T230000Z' + '\n' +
+'LOCATION:Hamilton Library' + '\n' +
+'SUMMARY;LANGUAGE=en-us:Study for Exam' + '\n' +
+'END:VEVENT' + '\n' +
+'END:VCALENDAR';
+
+var text={"myfile.ics":icsText};
 app.get('/files/:name',function(req,res){
     res.set({"Content-Disposition":"attachment; filename=\"myfile.ics\""});
     res.send(text[req.params.name]);
