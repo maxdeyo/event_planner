9162136b0f54853c19f71834b76e2c2ee4db27f0
Jolie <37143047+jolieching@users.noreply.github.com>
2020-07-30 07:44:18 -1000

    small syntax error

--- client/src/App.js
+++ client/src/App.js
@@ -51,7 +51,7 @@ class App extends Component {
     this.setState({event: {...this.state.event, dtend: endDate}})
   };
 
-  handlePriorityChange = endDate => {
+  handlePriorityChange = extraPriority => {
     this.setState({event: {...this.state.event, priority: extraPriority}})
   };
 
