a0c01e8294934cb2a24b6dbc837322b1627cc566
maxdeyo <maxdeyo@hawaii.edu>
2020-07-30 21:30:15 -1000

    some changes

--- client/src/components/EventCard.js
+++ client/src/components/EventCard.js
@@ -8,14 +8,29 @@ const icsText = (data) => {
   'VERSION:2.0' + '\n' + 
   'BEGIN:VEVENT' + '\n' + 
   'CLASS:PUBLIC' + '\n' + 
-  'DESCRIPTION:' + data.name.toString() + '\n' +
-  'DTSTART;TZID=' + data.tzid.toString() + ':' + data.dtstart.toString() + '\n' +
-  'DTEND;TZID=' + data.tzid.toString() + ':' + data.dtend.toString() + '\n' +
-  'RRULE:FREQ=' + data.recurrence.toString() + '\n' +
-  'LOCATION:' + data.location.toString() + '\n' +
-  'SUMMARY;LANGUAGE=en-us:' + data.description.toString() + '\n' +
-  'RESOURCES:' + data.resources.toString() + '\n' +
-  'PRIORITY:' + data.priority.toString() + '\n' +
+  'DESCRIPTION:' + data.name.toString() + '\n';
+
+  //Temp comment out tzid as it doesn't work
+    /*str+='DTSTART;TZID=' + data.tzid.toString() + ':' + data.dtstart.toString() + '\n' +
+    'DTEND;TZID=' + data.tzid.toString() + ':' + data.dtend.toString() + '\n';*/
+
+  str+='DTSTART:' + data.dtstart.toString() + '\n' +
+  'DTEND:' + data.dtend.toString() + '\n';
+  if(data.recurrence){
+    str += 'RRULE:FREQ=' + data.recurrence.toString() + '\n';
+  }
+  if(data.location){
+    str+='LOCATION:' + data.location.toString() + '\n'
+  }
+  str+=
+  'SUMMARY;LANGUAGE=en-us:' + data.description.toString() + '\n';
+  if(data.resources){
+    str += 'RESOURCES:' + data.resources.toString() + '\n';
+  }
+  if(data.priority){
+    str+='PRIORITY:' + data.priority.toString() + '\n';
+  }
+  str+=
   'END:VEVENT' + '\n' +
   'END:VCALENDAR';
    return str;
