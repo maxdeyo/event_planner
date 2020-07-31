29a11aae88c9448d1378c69a908a98e9cb18152b
Jolie <37143047+jolieching@users.noreply.github.com>
2020-07-31 08:44:02 -1000

    test

--- index.js
+++ index.js
@@ -58,14 +58,32 @@ const icsText = (data) => {
   'VERSION:2.0' + '\n' + 
   'BEGIN:VEVENT' + '\n' + 
   'CLASS:PUBLIC' + '\n' + 
-  'DESCRIPTION:' + data.name.toString() + '\n' +
-  'DTSTART:' + data.dtstart.toString() + '\n' +
-  'DTEND:' + data.dtend.toString() + '\n' +
-  'LOCATION:' + data.location.toString() + '\n' +
-  'SUMMARY;LANGUAGE=en-us:' + data.description.toString() + '\n' +
+  'DESCRIPTION:' + data.name.toString() + '\n';
+
+  //Temp comment out tzid as it doesn't work
+    /*str+='DTSTART;TZID=' + data.tzid.toString() + ':' + data.dtstart.toString() + '\n' +
+    'DTEND;TZID=' + data.tzid.toString() + ':' + data.dtend.toString() + '\n';*/
+
+  str+='DTSTART:' + data.dtstart.toString() + '\n' +
+  'DTEND:' + data.dtend.toString() + '\n';
+  if(data.recurrence){
+    str += 'RRULE:FREQ=' + data.recurrence.toString() + '\n';
+  }
+  if(data.location){
+    str+='LOCATION:' + data.location.toString() + '\n'
+  }
+  str+=
+  'SUMMARY;LANGUAGE=en-us:' + data.description.toString() + '\n';
+  if(data.resources){
+    str += 'RESOURCES:' + data.resources.toString() + '\n';
+  }
+  if(data.priority){
+    str+='PRIORITY:' + data.priority.toString() + '\n';
+  }
+  str+=
   'END:VEVENT' + '\n' +
   'END:VCALENDAR';
-  return str;
+   return str;
 }
 
 var text={"myfile.ics":"This is a file!"};
